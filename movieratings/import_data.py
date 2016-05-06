11fd1b8280acdeb619508cccf83dd5462968de29
Julio Cardoza <julio_jr3@hotmail.com>
2016-05-06 13:35:37 -0400

    created new data migrations to import data from csv files to DB tables

--- movieratings/import_data.py
+++ movieratings/import_data.py
@@ -1,51 +1,69 @@
 
 import csv
 import django
+#export DJANGO_SETTINGS_MODULE="movieratings.settings"  ----> Commandline instruction
+#echo $DJANGO_SETTINGS_MODULE
 django.setup()
 from movie_lens_online.models import Movie, Rater, Rating
 from django.utils import timezone
 import datetime
 
+Movie.objects.all().delete()
+Rater.objects.all().delete()
+Rating.objects.all().delete()
+
+
 
 #----------------------------------------------
-# Filling Movie table with dataset U.item
-# with open ('/Users/Pipita/movie_lenses/Data/u.item', encoding='latin_1') as movie_file:
+# #Filling Movie table with dataset U.item
+with open ('/Users/Pipita/movie_lenses/Data/u.item', encoding='latin_1') as movie_file:
+
+    movie_dataset = csv.DictReader(movie_file, fieldnames=('movie_id', 'movie_title', 'release_date'), delimiter="|")
+
+    for row in movie_dataset:
+
+        cln_date = datetime.datetime.strptime(row['release_date'], "%d-%b-%Y").date()
+
+        movie_row = Movie(movie_id=row['movie_id'], movie_title=row['movie_title'], release_date=cln_date) #action=row['action']
+        movie_row.save()
 #
-#     movie_dataset = csv.DictReader(movie_file, fieldnames=('movie_id', 'movie_title', 'release_date','1', '2','3', 'action',  ""), delimiter="|")
 #
-#     for row in movie_dataset:
-#
-#         cln_date = datetime.datetime.strptime(row['release_date'], "%d-%b-%Y").date()
-#
-#         movie_row = Movie(movie_id=row['movie_id'], movie_title=row['movie_title'], release_date=cln_date, action=row['action'])
-#         movie_row.save()
-# #
-#
-#-------------------------------------------
-# Filling Rater table with dataset U.user
-# with open ('/Users/Pipita/movie_lenses/Data/u.user') as user_file:
-#
-#     user_dataset = csv.DictReader(user_file, fieldnames=('user_id', 'age', 'gender','occupation', 'zip_code',  ""), delimiter="|")
-#
-#     for row_1 in user_dataset:
-#
-#         rater_row = Rater(user_id=row_1['user_id'], age=row_1['age'], gender=row_1['gender'], occupation=row_1['occupation'], zip_code=row_1['zip_code'])
-#         rater_row.save()
-#
-# #
-# #--------------------------------------------
-# #Filling Ratings table with dataset U.data
-# with open ('/Users/Pipita/movie_lenses/Data/u.data') as ratings_file:
-#
-#     ratings_dataset = csv.DictReader(ratings_file,  fieldnames=('user_id', 'movie_id', 'rating', ""), delimiter="\t")
-#
-#     for row_2 in ratings_dataset:
-#
-#         rating_row = Rating(rating=row_2['rating'], item_id=Movie.objects.get(id=row_2['movie_id']), user_id=Rater.objects.get(id=row_2['user_id']))
-#
-#         rating_row.save()
+# # -------------------------------------------
+# # Filling Rater table with dataset U.user
+with open ('/Users/Pipita/movie_lenses/Data/u.user') as user_file:
+
+    user_dataset = csv.DictReader(user_file, fieldnames=('user_id', 'age', 'sex','occupation',  ""), delimiter="|") # 'zip_code',
+
+    for row_1 in user_dataset:
+
+        rater_row = Rater(user_id=row_1['user_id'], age=row_1['age'], sex=row_1['sex'], occupation=row_1['occupation']) #zip_code=row_1['zip_code']
+        rater_row.save()
 
 
-rating_row = Rating(rating=3, item_id=Movie.objects.get(id=2297), user_id=Rater.objects.get(id=3959))
+# --------------------------------------------
+# Filling Ratings table with dataset U.data
+with open ('/Users/Pipita/movie_lenses/Data/u.data') as ratings_file:
 
-rating_row.save()
+    ratings_dataset = csv.DictReader(ratings_file,  fieldnames=('user_id', 'movie_id', 'rating', ""), delimiter="\t")
+
+    for row_2 in ratings_dataset:
+        movie = Movie.objects.get(movie_id=row_2['movie_id'])
+
+        rater = Rater.objects.get(user_id=row_2['user_id'])
+
+        rating_row = Rating(rating=row_2['rating'], item_id=movie, user_id=rater)
+
+        rating_row.save()
+
+
+#____________________________________________________________
+
+mov = Movie.objects.filter(movie_id = movie['movie_id'])
+movie['avg_rating']=rating['rating__avg']
+
+for movie in Movie.objects.values():
+    mov = Movie.objects.get(movie_id=movie['movie_id'])
+    print(mov.movie_id)
+    rating = Rating.objects.filter(movie_id=movie['movie_id']).aggregate(Avg('rating'))
+    mov.avg_rating = (rating['rating__avg'])
+    mov.save()
--- movieratings/movie_lens_online/models.py
+++ movieratings/movie_lens_online/models.py
@@ -1,27 +1,47 @@
+# from django.db import models
+#
+# # Create your models here.
+#
+# class Rater(models.Model):
+#     user_id = models.CharField(max_length=3)
+#     age = models.IntegerField()
+#     gender = models.CharField(max_length=1)
+#     occupation = models.CharField(max_length=200)
+#     zip_code = models.CharField(max_length=5)
+#
+#
+#
+# class Movie(models.Model):
+#     movie_id = models.CharField(max_length=6)
+#     movie_title = models.CharField(max_length=200)
+#     release_date = models.DateField('release_date')
+#     action = models.CharField(max_length=1)
+#     avg_rating = models.FloatField(default=0)
+#
+#
+# class Rating(models.Model):
+#     user_id = models.ForeignKey(Rater, on_delete=models.CASCADE)
+#     item_id = models.ForeignKey(Movie, on_delete=models.CASCADE)
+#     rating = models.IntegerField(default=0)
+
+#-------------------------------------
 from django.db import models
 
 # Create your models here.
-
 class Rater(models.Model):
-    user_id = models.CharField(max_length=3)
-    age = models.IntegerField()
-    gender = models.CharField(max_length=1)
-    occupation = models.CharField(max_length=200)
-    zip_code = models.CharField(max_length=5)
-
-
+    user_id = models.CharField(max_length=255, primary_key=True)
+    age = models.CharField(max_length=255)
+    sex = models.CharField(max_length=255, default ='')
+    occupation = models.CharField(max_length=255)
 
 class Movie(models.Model):
-    movie_id = models.CharField(max_length=6)
-    movie_title = models.CharField(max_length=200)
-    release_date = models.DateField('release_date')
-    action = models.CharField(max_length=1)
-
+    movie_id = models.CharField(max_length=255, primary_key=True)
+    movie_title = models.CharField(max_length=255)
+    release_date = models.CharField(max_length=255)
+    avg_rating = models.FloatField(default=0)
 
 class Rating(models.Model):
-    user_id = models.ForeignKey(Rater, on_delete=models.CASCADE)
-    item_id = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    # movie_id = models.CharField(max_length=255)
     rating = models.IntegerField(default=0)
-
-
-#timestamp.
+    rater = models.ForeignKey(Rater, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
