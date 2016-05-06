13be4abc59e23d545ba5a32e73ab6c9ed7825050
David Conley <conleydg@gmail.com>
2016-05-06 14:16:22 -0400

    added view to admin page

--- movieratings/movie_lens_online/admin.py
+++ movieratings/movie_lens_online/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Rater, Movie, Rating
+
+admin.site.register(Rater)
+admin.site.register(Movie)
+admin.site.register(Rating)
